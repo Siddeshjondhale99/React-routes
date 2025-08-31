2a8c5a00e7a48130fb6d8f3498a5f54483d631ac
Siddeshjondhale99 <siddeshjondhale99@gmail.com>
2025-08-31 22:53:45 +0530

    routes is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-orange-300">
         Hello world!
+        <Header />
+        <Home />
+        <Footer />
       </h1>
     </>
   );
